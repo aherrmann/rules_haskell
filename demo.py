f3ed1123ce58717f22410e164c733dde91eb870c
Andreas Herrmann <andreas.herrmann@tweag.io>
2019-07-24 17:35:40 +0200

    imports

--- demo.py
+++ demo.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 
 from bazel_tools.tools.python.runfiles import runfiles as bazel_runfiles
+from contextlib import contextmanager
+import glob
+import itertools
+import os
+import platform
+import shlex
+import subprocess
 import sys
+import tempfile
+
+import datetime
 
 outfile = open(sys.argv[1], "w")
 outfile.write(sys.argv[2])
